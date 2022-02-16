2e160e154848d01a0005b7e0009b5af915727197
Sebastián Gómez <sebaskahn@gmail.com>
2022-02-16 17:52:00 -0300

    We have added some functionalities: report handling.

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -10,6 +10,7 @@ import pages.BasePage;
 @CucumberOptions(
 		features = "src/test/resources/features",
 		glue = "steps",
+		plugin = {"pretty", "html:target/cucumber-report.html"},
 		monochrome = true,
 		tags = "@List"
 )
